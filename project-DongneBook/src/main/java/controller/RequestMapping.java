38f2e7243ebee9da60c484afdcceb9f3590f8d1e
juhee-dev <viimv2000@gmail.com>
2021-11-12 17:46:48 +0900

    ㅋㅋ오류ㅇㅇㅏㄴ뜬

--- project-DongneBook/src/main/java/controller/RequestMapping.java
+++ project-DongneBook/src/main/java/controller/RequestMapping.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import controller.user.*;
-import controller.comm.*;
 
 public class RequestMapping {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
@@ -20,8 +19,6 @@ public class RequestMapping {
         mappings.put("/user/login/form", new ForwardController("/user/loginForm.jsp"));
         mappings.put("/user/login", new LoginController());
         mappings.put("/user/logout", new LogoutController());
-        mappings.put("/user/list", new ListUserController());
-        mappings.put("/user/view", new ViewUserController());
         
         // 회원 가입 폼 요청과 가입 요청 처리 병합 (폼에 커뮤니티 선택 메뉴 추가를 위함)
 //      mappings.put("/user/register/form", new ForwardController("/user/registerForm.jsp"));
@@ -35,12 +32,12 @@ public class RequestMapping {
         
         mappings.put("/user/delete", new DeleteUserController());
         
-        // 커뮤니티 관련 request URI 추가
-        mappings.put("/community/list", new ListCommunityController());
-        mappings.put("/community/view", new ViewCommunityController());
-        mappings.put("/community/create/form", new ForwardController("/community/creationForm.jsp"));
-        mappings.put("/community/create", new CreateCommunityController());
-        mappings.put("/community/update", new UpdateCommunityController());
+        // book 관련 request URI 추가 수정해야함
+//        mappings.put("/community/list", new ListCommunityController());
+//        mappings.put("/community/view", new ViewCommunityController());
+//        mappings.put("/community/create/form", new ForwardController("/community/creationForm.jsp"));
+//        mappings.put("/community/create", new CreateCommunityController());
+//        mappings.put("/community/update", new UpdateCommunityController());
         
         logger.info("Initialized Request Mapping!");
     }
