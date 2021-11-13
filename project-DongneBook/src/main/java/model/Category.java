d5ce8e9b9e71080dbf23ba83b596088ca5f26771
chashudong <67837142+chashudong@users.noreply.github.com>
2021-11-13 20:54:58 +0900

    Create Category.java
    변수 이름에 cate를 추가하고, 생성자를 추가했어요!

---
+++ project-DongneBook/src/main/java/model/Category.java
@@ -0,0 +1,28 @@
+public class Category {
+    
+    private long cateId;
+    private String cateName;
+
+    public Category() { }		// 기본 생성자
+	
+	public Category(long cateId, String cateName) {
+		this.cateId = cateId;
+		this.cateName = cateName;
+	}
+
+	public long getCateId() {
+		return cateId;
+	}
+
+	public void setCateId(long cateId) {
+		this.cateId = cateId;
+	}
+
+	public String getCateName() {
+		return cateName;
+	}
+
+	public void setCateName(String cateName) {
+		this.cateName = cateName;
+	}
+}
