fe337c686177978185cff4f99bd7b9a6750bf06f
Yujin <91461061+HeoYuJinn@users.noreply.github.com>
2021-11-14 16:35:47 +0900

    bookDAO
    메소드추가

--- project-DongneBook/src/main/java/model/dao/BookDAO.java
+++ project-DongneBook/src/main/java/model/dao/BookDAO.java
@@ -22,9 +22,9 @@ public class BookDAO {
 	// 새로운 책 생성	
 	public int create(Book book) throws SQLException {
 		String sql = "INSERT INTO BOOK VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-		Object [] param = new Object[] { book.getTitle(), book.getAuthors(),
-						book.getPublisher(), book.getProductidentifier(), book.getPrice(),
-						book.getDescription(), book.getImage(), book.getcateId(), book.getuserId(), book.getBookId() }; 
+		Object [] param = new Object[] { book.getTitle(), book.getauthor(),
+						book.getPublisher(), book.getPublicationDate(), book.getPrice(),
+						book.getDescription(), book.getImage(), book.getgetCateId(), book.getUserId(), book.getBookId() }; 
 		
 		jdbcUtil.setSqlAndParameters(sql, param);	// JDBCUtil 에 insert문과 매개 변수 설정
 		
@@ -44,11 +44,11 @@ public class BookDAO {
 	
 	//메인에서 이용할 책 찾기 (책 이미지, 제목, 가격) -> findUerList 참고함.
 	public List<Book> findBookMain() throws SQLException {
-		// BOOK에서 .. bookId를 통해 이미지(주소), 제목, 가격 가져온다.
+		// BOOK에서 .. book_id를 통해 이미지(주소), 제목, 가격 가져온다.
 		// 쿼리문이~ 확실치 않아요~
         String sql = "SELECT image, title, price "  
         		   + "FROM BOOK "
-        		+ "WHERE bookId=? "; 
+        		+ "WHERE book_id=? "; 
 		jdbcUtil.setSqlAndParameters(sql, null);		// JDBCUtil에 query문 설정
 					
 		try {
@@ -56,17 +56,10 @@ public class BookDAO {
 			List<User> BookMainList = new ArrayList<User>();	// User들의 리스트 생성
 			while (rs.next()) {
 				Book user = new Book(			// User 객체를 생성하여 현재 행의 정보를 저장
-					rs.getString("bookId"),
-					null,
-					null,
+					rs.getString("book_id"),
 					rs.getString("title"),
-					null,
-					null,
-					null,
-					null,
-					rs.getString("price"),
-					null,
-					rs.getShort("image"));
+					rs.getInt("price"),
+					rs.getString("image"));
 				BookMainList.add(book);				// List에 Book 객체 저장
 			}		
 			return BookMainList;					
@@ -80,25 +73,25 @@ public class BookDAO {
 	}
 	
 	
-	//책 상세정보 보기에서 사용할 Find. (userid를 포함한 책 정보 전부)
-	public Book findBookDetails(String bookId) throws SQLException {
-        String sql = "SELECT userid, bookid, cateid, title, authors, publisher,"
+	//책 상세정보 보기에서 사용할 Find. (user_id를 포함한 책 정보 전부)
+	public Book findBookDetails(String book_id) throws SQLException {
+        String sql = "SELECT user_id, book_id, category_id, title, author, publisher,"
         		+ " publicationdate, price, description, image "
         			+ "FROM BOOK "
-        			+ "WHERE bookid=? ";              
-		jdbcUtil.setSqlAndParameters(sql, new Object[] {userId});	// JDBCUtil에 query문과 매개 변수 설정
+        			+ "WHERE book_id=? ";              
+		jdbcUtil.setSqlAndParameters(sql, new Object[] {book_id});	// JDBCUtil에 query문과 매개 변수 설정
 
 		try {
 			ResultSet rs = jdbcUtil.executeQuery();		// query 실행
 			if (rs.next()) {						// 책 정보 발견
 				Book book = new Book(		// User 객체를 생성하여 학생 정보를 저장
-					bookId,
-					rs.getInt("userid"),
-					rs.getString("cateid"),
+					book_id,
+					rs.getInt("user_id"),
+					rs.getString("category_id"),
 					rs.getString("title"),
-					rs.getString("authors"),
+					rs.getString("author"),
 					rs.getString("publisher"),					
-					rs.getDate("publicationdate"),
+					rs.getDate("publication_date"),
 					rs.getInt("price"),
 					rs.getString("description"),
 					rs.getString("image")
@@ -114,13 +107,14 @@ public class BookDAO {
 	}
 	
 	/**
-	 * 북 ID에 해당하는 사용자를 삭제.
+	 * 북 ID에 해당하는 사용자를 삭제. 
 	 */
-	public int remove(String bookId) throws SQLException {
+	public int deleteBook(String book_id) throws SQLException {
+		//보류
 		String sql = "DELETE book, condition "
 				+ "FROM book JOIN condition USING(book_id) "
 				+ "WHERE book_id=?";		
-		jdbcUtil.setSqlAndParameters(sql, new Object[] {bookId});	// JDBCUtil에 delete문과 매개 변수 설정
+		jdbcUtil.setSqlAndParameters(sql, new Object[] {book_id});	// JDBCUtil에 delete문과 매개 변수 설정
 
 		try {				
 			int result = jdbcUtil.executeUpdate();	// delete 문 실행
