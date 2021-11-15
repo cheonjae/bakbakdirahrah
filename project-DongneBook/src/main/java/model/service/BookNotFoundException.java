fcf3f8cb0af46abb0bcd8978a218722477fdab96
juhee-dev <56381189+juhee-dev@users.noreply.github.com>
2021-11-15 18:37:54 +0900

    Add files via upload

---
+++ project-DongneBook/src/main/java/model/service/BookNotFoundException.java
@@ -0,0 +1,15 @@
+package model.service;
+
+public class BookNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public BookNotFoundException() {
+		super();
+	}
+
+	public BookNotFoundException(String arg0) {
+		super(arg0);
+	}
+	
+}
