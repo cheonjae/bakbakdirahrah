991cea5585459cb5d2645f1cb1a01b6a16e5eae3
이가은 <67837142+chashudong@users.noreply.github.com>
2021-11-17 14:54:35 +0900

    Update BookDetailController.java
    북 디테일 컨트롤러 추가

--- project-DongneBook/src/main/java/controller/book/BookDetailController.java
+++ project-DongneBook/src/main/java/controller/book/BookDetailController.java
@@ -1,10 +1,29 @@
 package controller.book;
 
-public class BookDetailController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-	}
+import controller.Controller;
+import model.service.BookManager;
+import model.Book;
 
+public class BookDetailController implements Controller {
+	private static final Logger log = LoggerFactory.getLogger(BookDetailController.class);
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response)	throws Exception {
+    	String bookId = request.getParameter("bookId");
+    	log.debug("BookDetail's BookId : {}", bookId);
+
+		BookManager manager = BookManager.getInstance();
+		Book book = manager.findBookDetails(bookId);
+		request.setAttribute("book", book);	
+		
+		// 북 디테일 화면으로 이동(forwarding)
+		return "/book/detail.jsp";
+    }
 }
