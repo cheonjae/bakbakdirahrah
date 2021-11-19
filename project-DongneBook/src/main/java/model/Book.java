e8b092f7478b2630e8db632a6299b0a6dbd35301
juhee-dev <56381189+juhee-dev@users.noreply.github.com>
2021-11-19 11:24:08 +0900

    수정) condition 정보 book 클래스에 포함

--- project-DongneBook/src/main/java/model/Book.java
+++ project-DongneBook/src/main/java/model/Book.java
@@ -1,8 +1,9 @@
-//梨� 愿�由щ�� �쐞�빐 �븘�슂�븳 �룄硫붿씤 �겢�옒�뒪.
+
 package model;
 
 import java.util.Date;
 
+// 책 관리를 위해 필요한 도메인 클래스. Book 테이블과 대응됨
 public class Book {
 	private int bookId;
 	private String userId;
@@ -15,11 +16,25 @@ public class Book {
 	private String description;
 	private String image;
 	private int sold;
-	
-	public Book() {} 	// 기본 생성자 
-	
-	public Book(int bookId, String userId, String title, String author,
-			String publisher, Date publicationDate,
+
+//	condition info
+	private int pageDiscoloration; // 페이지 변색 - 0(없음) or 1(있음)
+	private int coverDamage; // 겉표지 - 0(깨끗하지 않음) or 1(깨끗함)
+	private int pageDamage; // 페이지 훼손 - 0(없음) or 1(있음)
+	private int writing; // 필기 흔적 - 0(없음) or 1(연필/샤프) or 2(볼펜/형광펜)
+
+	public Book() {
+	} // 기본 생성
+
+	// 책 제목, 가격, 이미지만 보여주기 위해서 만든..생성자..
+	public Book(int bookId, String title, int price, String image) {
+		this.bookId = bookId;
+		this.title = title;
+		this.price = price;
+		this.image = image;
+	}
+
+	public Book(int bookId, String userId, String title, String author, String publisher, Date publicationDate,
 			int price, String description, String image, int sold, int cateId) {
 		this.bookId = bookId;
 		this.userId = userId;
@@ -33,16 +48,17 @@ public class Book {
 		this.sold = sold;
 		this.cateId = cateId;
 	}
-	
-	//책 제목, 가격, 이미지만 보여주기 위해서 만든..생성자..
-	public Book(int bookId, String title, int price, String image) {
-		this.bookId = bookId;
-		this.title = title;
-		this.price = price;
-		this.image = image;
+
+	public Book(int bookId, String userId, String title, String author, String publisher, Date publicationDate,
+				int price, String description, String image, int sold, int cateId,
+				int pageDiscoloration, int coverDamage, int pageDamage, int writing) {
+		this(bookId, userId, title, author, publisher, publicationDate, price, description, image, sold, cateId);
+		pageDiscoloration = this.pageDiscoloration;
+		coverDamage = this.coverDamage;
+		pageDamage = this.pageDamage;
+		writing = this.writing;
 	}
 
-	
 	public int getBookId() {
 		return bookId;
 	}
@@ -122,20 +138,52 @@ public class Book {
 	public void setImage(String image) {
 		this.image = image;
 	}
-	
+
 	public int getSold() {
 		return sold;
 	}
-	
+
 	public void setSold(int sold) {
 		this.sold = sold;
 	}
 
+	public int getPageDiscoloration() {
+		return pageDiscoloration;
+	}
+
+	public void setPageDiscoloration(int pageDiscoloration) {
+		this.pageDiscoloration = pageDiscoloration;
+	}
+
+	public int getCoverDamage() {
+		return coverDamage;
+	}
+
+	public void setCoverDamage(int coverDamage) {
+		this.coverDamage = coverDamage;
+	}
+
+	public int getPageDamage() {
+		return pageDamage;
+	}
+
+	public void setPageDamage(int pageDamage) {
+		this.pageDamage = pageDamage;
+	}
+
+	public int getWriting() {
+		return writing;
+	}
+
+	public void setWriting(int writing) {
+		this.writing = writing;
+	}
+
 	@Override
 	public String toString() {
-		return "Book [bookId=" + bookId + ", userId=" + userId + ", cateId=" + cateId + ", title=" + title
-				+ ", author=" + author + ", publisher=" + publisher + ", publicationDate=" + publicationDate
-				+ ", price=" + price + ", description=" + description + ", image=" + image + ", sold=" + sold + "]";
+		return "Book [bookId=" + bookId + ", userId=" + userId + ", cateId=" + cateId + ", title=" + title + ", author="
+				+ author + ", publisher=" + publisher + ", publicationDate=" + publicationDate + ", price=" + price
+				+ ", description=" + description + ", image=" + image + ", sold=" + sold + "]";
 	}
-	
+
 }
