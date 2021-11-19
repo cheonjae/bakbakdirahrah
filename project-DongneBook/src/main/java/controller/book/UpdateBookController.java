798997d047f89e19fa6a035291655d86b56ddf6e
Yujin <91461061+HeoYuJinn@users.noreply.github.com>
2021-11-19 23:10:24 +0900

    Update UpdateBookController.java

--- project-DongneBook/src/main/java/controller/book/UpdateBookController.java
+++ project-DongneBook/src/main/java/controller/book/UpdateBookController.java
@@ -21,19 +21,23 @@ public class UpdateBookController implements Controller{
     public String execute(HttpServletRequest request, HttpServletResponse response)	throws Exception {
 		if (request.getMethod().equals("GET")) {	
     		
+			//update할 책 Id, update 요청한 유저 Id 받아옴. 
     		String updateId = request.getParameter("bookId"); 
     		String updateUserId = request.getParameter("UserId"); 
     		
     		//log.debug("BookUpdateForm Request : {}", updateId);
  
     		BookManager bookmanager = BookManager.getInstance();
-			java.awt.print.Book book = bookmanager.findBookDetails(updateId);	// 수정하려는 책 정보 검색
+    		
+    		// 수정하려는 책 정보를 Bookmanager에 findBookDetails를 통해서 불러옴.
+			Book book = bookmanager.findBookDetails(updateId);	
+			// ↑그 수정하려는 책 정보가 들어있는 book 객체
 			request.setAttribute("book", book);		
 			
-	
 
 			HttpSession session = request.getSession();
-			if (UserSessionUtils.isLoginUser(updateUserId, session) || //응
+			
+			if (UserSessionUtils.isLoginUser(updateUserId, session) || 
 				UserSessionUtils.isLoginUser("admin", session)) {
 				// 현재 로그인한 사용자가 수정 대상 사용자이거나 관리자인 경우 -> 수정 가능
 				return "/book/updateForm.jsp";   // 검색한 책정보를 상페로 전송     
@@ -45,6 +49,20 @@ public class UpdateBookController implements Controller{
 					new IllegalStateException("타인의 책은 수정할 수 없습니다."));            
 			return "/book/detail.jsp";	// 상세페이지로이동 (forwarding)
 	    }
+		
+		// POST request. Book 도클에 있는 가격 설명 팔림 생성자 이용함
+    	Book updateBook = new Book(
+    		Integer.parseInt(request.getParameter("bookId"),
+    		Integer.parseInt(request.getParameter("price"),
+    		request.getParameter("description"),
+    		Integer.parseInt(request.getParameter("sold"));
+			);
+
+    	//log.debug("Update User : {}", updateUser);
+
+		BookManager manager = BookManager.getInstance();
+		manager.update(updateBook);			
+        return "redirect:book/detail"; 
 		
     		
     }	 	
