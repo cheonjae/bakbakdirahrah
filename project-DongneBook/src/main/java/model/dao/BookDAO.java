aad42a40114c4075e728161d1f0aa2e792f48704
이가은 <67837142+chashudong@users.noreply.github.com>
2021-11-20 15:47:05 +0900

    크리에이트문수정

--- project-DongneBook/src/main/java/model/dao/BookDAO.java
+++ project-DongneBook/src/main/java/model/dao/BookDAO.java
@@ -21,10 +21,10 @@ public class BookDAO {
 		
 	// 새로운 책 생성	
 	public int create(Book book) throws SQLException {
-		String sql = "INSERT INTO BOOK VALUES (?, ?, ?, BOOKSEQ.nextval, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"; // 카테id도 sequence 
-		Object [] param = new Object[] { book.getTitle(), book.getauthor(),
-						book.getPublisher(), book.getBookId(), book.getPrice(),
-						book.getDescription(), book.getImage(), book.getUserId(), book.getSold(), book.getCateId(),
+		String sql = "INSERT INTO BOOK VALUES (?, ?, ?, BOOKSEQ.nextval, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		Object [] param = new Object[] { book.getTitle(), book.getauthor(), book.getPublisher(),
+						book.getPrice(), book.getDescription(), book.getImage(),
+						book.getUserId(), book.getSold(), book.getCateId(),
 						book.getPageDiscoloration(), book.getCoverDamage(), book.getPageDamage(), book.getWriting()}; 
 		
 		jdbcUtil.setSqlAndParameters(sql, param);	// JDBCUtil 에 insert문과 매개 변수 설정
