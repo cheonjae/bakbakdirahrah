bc389be7f8e24060ecfb0de550745b74f871427b
이가은 <67837142+chashudong@users.noreply.github.com>
2021-11-20 23:38:30 +0900

    Update RegisterUserController.java

--- project-DongneBook/src/main/java/controller/user/RegisterUserController.java
+++ project-DongneBook/src/main/java/controller/user/RegisterUserController.java
@@ -17,12 +17,12 @@ public class RegisterUserController implements Controller {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
        	if (request.getMethod().equals("GET")) {	
-    		// GET request: 회원정보 등록 form 요청	
+	
     		log.debug("RegisterForm Request");
-			return "/user/registerForm.jsp";   // 검색한 사용자 정보를 update form으로 전송     	
+			return "/user/userRegisterForm.jsp";   	
 	    }	
 
-    	// POST request (회원정보가 parameter로 전송됨)
+    	// POST request
        	User user = new User(
 			request.getParameter("userId"),
 			request.getParameter("password"),
@@ -36,13 +36,13 @@ public class RegisterUserController implements Controller {
 		try {
 			UserManager manager = UserManager.getInstance();
 			manager.create(user);
-	        return "redirect:/user/list";	// 성공 시 사용자 리스트 화면으로 redirect
+	        return "redirect:/user/main";
 	        
-		} catch (ExistingUserException e) {	// 예외 발생 시 회원가입 form으로 forwarding
+		} catch (ExistingUserException e) {	
             request.setAttribute("registerFailed", true);
 			request.setAttribute("exception", e);
 			request.setAttribute("user", user);
-			return "/user/registerForm.jsp";
+			return "/user/userRegisterForm.jsp";
 		}
     }
 }
