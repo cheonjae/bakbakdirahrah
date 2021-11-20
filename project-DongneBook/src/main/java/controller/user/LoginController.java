8597a8a7dcf30761305811c32173664e1ac6242a
juhee-dev <56381189+juhee-dev@users.noreply.github.com>
2021-11-20 23:09:59 +0900

    Update LoginController.java

--- project-DongneBook/src/main/java/controller/user/LoginController.java
+++ project-DongneBook/src/main/java/controller/user/LoginController.java
@@ -18,14 +18,19 @@ public class LoginController implements Controller {
 		String password = request.getParameter("password");
 		
 		try {
+			// 紐⑤뜽�뿉 濡쒓렇�씤 泥섎━瑜� �쐞�엫
 			UserManager manager = UserManager.getInstance();
 			manager.login(userId, password);
 	
+			// �꽭�뀡�뿉 �궗�슜�옄 �씠�씠�뵒 ���옣
 			HttpSession session = request.getSession();
             session.setAttribute(UserSessionUtils.USER_SESSION_KEY, userId);
             
             return "redirect:/user/main";			
 		} catch (Exception e) {
+			/* UserNotFoundException�씠�굹 PasswordMismatchException 諛쒖깮 �떆
+			 * �떎�떆 login form�쓣 �궗�슜�옄�뿉寃� �쟾�넚�븯怨� �삤瑜� 硫붿꽭吏��룄 異쒕젰
+			 */
             request.setAttribute("loginFailed", true);
 			request.setAttribute("exception", e);
 			request.setCharacterEncoding("UTF-8");
