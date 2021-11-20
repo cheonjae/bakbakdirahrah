da3132478cb364d508633c09e9a436cf5b676002
cheonjae <69137343+cheonjae@users.noreply.github.com>
2021-11-20 23:01:13 +0900

    Update LoginController.java

--- project-DongneBook/src/main/java/controller/user/LoginController.java
+++ project-DongneBook/src/main/java/controller/user/LoginController.java
@@ -1,11 +1,15 @@
 package controller.user;
 
+import java.io.UnsupportedEncodingException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import controller.Controller;
+import model.service.PasswordMismatchException;
 import model.service.UserManager;
+import model.service.UserNotFoundException;
 
 public class LoginController implements Controller {
     @Override
@@ -14,21 +18,17 @@ public class LoginController implements Controller {
 		String password = request.getParameter("password");
 		
 		try {
-			// 모델에 로그인 처리를 위임
 			UserManager manager = UserManager.getInstance();
 			manager.login(userId, password);
 	
-			// 세션에 사용자 이이디 저장
 			HttpSession session = request.getSession();
             session.setAttribute(UserSessionUtils.USER_SESSION_KEY, userId);
             
-            return "redirect:/user/list";			
+            return "redirect:/user/main";			
 		} catch (Exception e) {
-			/* UserNotFoundException이나 PasswordMismatchException 발생 시
-			 * 다시 login form을 사용자에게 전송하고 오류 메세지도 출력
-			 */
             request.setAttribute("loginFailed", true);
 			request.setAttribute("exception", e);
+			request.setCharacterEncoding("UTF-8");
             return "/user/loginForm.jsp";			
 		}	
     }
