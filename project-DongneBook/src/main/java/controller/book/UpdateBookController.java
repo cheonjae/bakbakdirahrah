e37a940dd586be41f885a424a5e92a27ec4db190
cheonjae <69137343+cheonjae@users.noreply.github.com>
2021-11-21 18:17:14 +0900

    Update UpdateBookController.java

--- project-DongneBook/src/main/java/controller/book/UpdateBookController.java
+++ project-DongneBook/src/main/java/controller/book/UpdateBookController.java
@@ -19,30 +19,25 @@ public class UpdateBookController implements Controller{
 	
 	@Override
     public String execute(HttpServletRequest request, HttpServletResponse response)	throws Exception {
-		if (request.getMethod().equals("GET")) {	
-    		
+	if (request.getMethod().equals("GET")) {	
     		String updateId = request.getParameter("bookId"); 
- 
-    		BookManager bookmanager = BookManager.getInstance();
-			Book book = bookmanager.findBookDetails(Integer.parseInt(updateId));	
-
-			request.setAttribute("book", book);		
-			return "/book/updateForm.jsp";
-	    }
 		
+		BookManager bookmanager = BookManager.getInstance();
+		Book book = bookmanager.findBookDetails(Integer.parseInt(updateId));	
 
+		request.setAttribute("book", book);		
+		return "/book/updateForm.jsp";
+	}
     	Book updateBook = new Book(
     		Integer.parseInt(request.getParameter("bookId")),
     		Integer.parseInt(request.getParameter("price")),
     		request.getParameter("description"),
     		Integer.parseInt(request.getParameter("sold"))
-			);
+	);
 
-		BookManager manager = BookManager.getInstance();
-		manager.update(updateBook);			
+	BookManager manager = BookManager.getInstance();
+	manager.update(updateBook);
+	    
         return "redirect:/user/main"; 
-		
-    		
     }	 	
-
 }
