a677ff692d60c66ef9eaf0e4ecb53e5e63955ae5
cheonjae <69137343+cheonjae@users.noreply.github.com>
2021-11-22 00:59:12 +0900

    이제 됩니다 ^^

--- project-DongneBook/src/main/java/controller/book/UpdateBookController.java
+++ project-DongneBook/src/main/java/controller/book/UpdateBookController.java
@@ -18,25 +18,30 @@ public class UpdateBookController implements Controller{
 	
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-	if (request.getMethod().equals("GET")) {	
-    		String updateId = request.getParameter("bookId"); 
+    	
+		if (request.getMethod().equals("GET")) {	
+	    		String updateId = request.getParameter("bookId"); 
+			
+			BookManager bookmanager = BookManager.getInstance();
+			Book book = bookmanager.findBookDetails(Integer.parseInt(updateId));	
+	
+			request.setAttribute("book", book);		
+			return "/book/updateForm.jsp";
+		}
+		int bookId = Integer.parseInt(request.getParameter("bookId"));
+		int price = Integer.parseInt(request.getParameter("price"));
+		String description = request.getParameter("description");
+		int sold = Integer.parseInt(request.getParameter("sold"));
 		
-		BookManager bookmanager = BookManager.getInstance();
-		Book book = bookmanager.findBookDetails(Integer.parseInt(updateId));	
 
-		request.setAttribute("book", book);		
-		return "/book/updateForm.jsp";
-	}
-    	Book updateBook = new Book(
-    		Integer.parseInt(request.getParameter("bookId")),
-    		Integer.parseInt(request.getParameter("price")),
-    		request.getParameter("description"),
-    		Integer.parseInt(request.getParameter("sold"))
-	);
+    	Book updateBook = new Book(bookId, price, description, sold);
 
-	BookManager manager = BookManager.getInstance();
-	manager.update(updateBook);
-	    
-        return "redirect:/user/main"; 
+		BookManager manager = BookManager.getInstance();
+		manager.update(updateBook);
+		
+		Book book = manager.findBookDetails(bookId);
+		request.setAttribute("book", book);
+
+		return "/book/detail.jsp";
     }	 	
 }
