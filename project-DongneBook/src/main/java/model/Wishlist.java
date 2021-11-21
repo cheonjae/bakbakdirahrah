dff7458caeffda562504c80bf0058476a6c94a41
juhee-dev <56381189+juhee-dev@users.noreply.github.com>
2021-11-21 13:30:18 +0900

    Create Wishlist.java

---
+++ project-DongneBook/src/main/java/model/Wishlist.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Wishlist {
+
+	private String userId;
+	private int bookId;
+	
+	public Wishlist(String userId, int bookId) {
+		super();
+		this.userId = userId;
+		this.bookId = bookId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	
+	
+}
