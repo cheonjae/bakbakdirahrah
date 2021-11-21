711aebbe6f56d9310a55a75de0cd31b8fae25e02
juhee-dev <56381189+juhee-dev@users.noreply.github.com>
2021-11-21 13:35:13 +0900

    Update Transaction.java

--- project-DongneBook/src/main/java/model/Transaction.java
+++ project-DongneBook/src/main/java/model/Transaction.java
@@ -5,14 +5,23 @@ public class Transaction {
 	private int bookId;
 	private String sellerId;
 	private String buyerId;
+	private int lastPrice;
+	private String meetingDate;
+	private String meetingPlace;
+	private String meetingMemo;
 	
 	public Transaction() {	}
-	
-	public Transaction(int bookId, String sellerId, String buyerId) {
+
+	public Transaction(int bookId, String sellerId, String buyerId, int lastPrice, String meetingDate,
+			String meetingPlace, String meetingMemo) {
 		super();
 		this.bookId = bookId;
 		this.sellerId = sellerId;
 		this.buyerId = buyerId;
+		this.lastPrice = lastPrice;
+		this.meetingDate = meetingDate;
+		this.meetingPlace = meetingPlace;
+		this.meetingMemo = meetingMemo;
 	}
 
 	public int getBookId() {
@@ -37,6 +46,38 @@ public class Transaction {
 
 	public void setBuyerId(String buyerId) {
 		this.buyerId = buyerId;
+	}
+
+	public int getLastPrice() {
+		return lastPrice;
+	}
+
+	public void setLastPrice(int lastPrice) {
+		this.lastPrice = lastPrice;
+	}
+
+	public String getMeetingDate() {
+		return meetingDate;
+	}
+
+	public void setMeetingDate(String meetingDate) {
+		this.meetingDate = meetingDate;
+	}
+
+	public String getMeetingPlace() {
+		return meetingPlace;
+	}
+
+	public void setMeetingPlace(String meetingPlace) {
+		this.meetingPlace = meetingPlace;
+	}
+
+	public String getMeetingMemo() {
+		return meetingMemo;
+	}
+
+	public void setMeetingMemo(String meetingMemo) {
+		this.meetingMemo = meetingMemo;
 	}
 	
 }
