6cae8bf655c34659d0793d8bda2db17033724d8f
이가은 <67837142+chashudong@users.noreply.github.com>
2021-11-24 04:15:49 +0900

    Update RequestMapping.java
    import controller.chat.*;

--- project-DongneBook/src/main/java/controller/RequestMapping.java
+++ project-DongneBook/src/main/java/controller/RequestMapping.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 
 import controller.user.*;
 import controller.book.*;
+import controller.chat.*;
 
 public class RequestMapping {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
