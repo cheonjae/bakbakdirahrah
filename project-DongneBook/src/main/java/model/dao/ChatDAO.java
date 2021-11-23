b30e690b13d5d92a8f6d8de90f0e917c94531dd3
cheonjae <69137343+cheonjae@users.noreply.github.com>
2021-11-24 01:05:40 +0900

    findChatContents (ViewChatContents) 추가

--- project-DongneBook/src/main/java/model/dao/ChatDAO.java
+++ project-DongneBook/src/main/java/model/dao/ChatDAO.java
@@ -1,5 +1,43 @@
 package model.dao;
 
-public class ChatDAO {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import model.Chat;
+
+public class ChatDAO {
+	private JDBCUtil jdbcUtil = null;
+	
+	public ChatDAO() {			
+		jdbcUtil = new JDBCUtil();	
+	}
+	
+	public List<Chat> findChatContents(String userId, String buddyId) throws SQLException {
+        String sql = "SELECT sender_id, receiver_id, contents "  
+        		   + "FROM chat "
+        		   + "WHERE (sender_id=? AND receiver_id=?) OR (sender_id=? AND receiver_id=?) "
+        		   + "ORDER BY created_at"; 
+		jdbcUtil.setSqlAndParameters(sql, new Object[] {userId, buddyId, buddyId, userId});	
+					
+		try {
+			ResultSet rs = jdbcUtil.executeQuery();				
+			List<Chat> chatContentsList = new ArrayList<Chat>();	
+			while (rs.next()) {
+				Chat chat = new Chat(			
+					rs.getString("contents"),
+					rs.getString("sender_id"),
+					rs.getString("receiver_id"));
+					chatContentsList.add(chat);	
+			}	
+			return chatContentsList;					
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			jdbcUtil.close();
+		}
+		return null;
+	}
+	
 }
