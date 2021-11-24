3536cff8404414980fdd2dcca267229099a25cf6
juhee-dev <56381189+juhee-dev@users.noreply.github.com>
2021-11-24 12:57:14 +0900

    chatList 매핑추가

--- project-DongneBook/src/main/java/controller/RequestMapping.java
+++ project-DongneBook/src/main/java/controller/RequestMapping.java
@@ -12,12 +12,9 @@ import controller.chat.*;
 public class RequestMapping {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
     
-    // �뜝�룞�삕 �뜝�룞�삕泥� uri�뜝�룞�삕 �뜝�룞�삕�뜝�룞�삕 controller �뜝�룞�삕泥닷뜝�룞�삕 �뜝�룞�삕�뜝�룞�삕�뜝�룞�삕 HashMap �뜝�룞�삕�뜝�룞�삕
     private Map<String, Controller> mappings = new HashMap<String, Controller>();
 
     public void initMapping() {
-    	// �뜝�룞�삕 uri�뜝�룞�삕 �뜝�룞�삕�뜝�룞�삕�뜝�떎�뙋�삕 controller �뜝�룞�삕泥닷뜝�룞�삕 �뜝�룞�삕�뜝�룞�삕 �뜝�룞�삕 �뜝�룞�삕�뜝�룞�삕
-//        mappings.put("/", new ForwardController("index.jsp"));
     	mappings.put("/user/main", new MainBookListController());
         mappings.put("/user/login/form", new ForwardController("/user/loginForm.jsp"));
         mappings.put("/user/login", new LoginController());
@@ -25,7 +22,6 @@ public class RequestMapping {
         mappings.put("/user/register/form", new ForwardController("/user/userRegisterForm.jsp"));
         mappings.put("/user/register", new RegisterUserController());
      
-        // �쉶�뜝�룞�삕 �뜝�룞�삕�뜝�룞�삕 �뜝�룞�삕 �뜝�룞�삕泥��뜝�룞�삕 �뜝�룞�삕�뜝�룞�삕 �뜝�룞�삕泥� 泥섇뜝�룞�삕 �뜝�룞�삕�뜝�룞�삕 (�뜝�룞�삕�뜝�룞�삕 而ㅵ뜝�듅�뙋�삕�떚 �뜝�룞�삕�뜝�룞�삕 �뜝�뙣�뙋�삕 �뜝�뙥怨ㅼ삕�뜝�룞�삕 �뜝�룞�삕�뜝�룞�삕)
         mappings.put("/book/register/form", new ForwardController("/book/bookRegisterForm.jsp"));
         mappings.put("/book/register", new RegisterBookController());
         mappings.put("/book/delete", new DeleteBookController());
@@ -35,12 +31,12 @@ public class RequestMapping {
         mappings.put("/book/update", new UpdateBookController()); 
         
         mappings.put("/chat/view", new ViewChatController());
+        mappings.put("/chat/list", new ListChatController());
         
         logger.info("Initialized Request Mapping!");
     }
 
     public Controller findController(String uri) {	
-    	// �뜝�뙇�뼲�삕�뜝�룞�삕 uri�뜝�룞�삕 �뜝�룞�삕�뜝�룞�삕�뜝�떎�뙋�삕 controller �뜝�룞�삕泥닷뜝�룞�삕 李얍뜝�룞�삕 �뜝�룞�삕�솚
-        return mappings.get(uri);
+    	return mappings.get(uri);
     }
 }
