a8566b11c1d6d70e6658feb5eb844c0a0d751985
juhee-dev <56381189+juhee-dev@users.noreply.github.com>
2021-11-24 23:49:06 +0900

    Update ListChatController.java

--- project-DongneBook/src/main/java/controller/chat/ListChatController.java
+++ project-DongneBook/src/main/java/controller/chat/ListChatController.java
@@ -9,17 +9,12 @@ import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import controller.Controller;
 import controller.user.UserSessionUtils;
 import model.service.ChatManager;
 
 public class ListChatController implements Controller {
-    private static final Logger log = LoggerFactory.getLogger(ListChatController.class);
-
-    @Override
+   @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
     	
     	ChatManager manager = ChatManager.getInstance();
@@ -32,7 +27,7 @@ public class ListChatController implements Controller {
     		}
     	}
     	buddyList = buddyList.stream().distinct().collect(Collectors.toList());
-			
+		
 		request.setAttribute("buddyList", buddyList);
 		return "/chat/chatList.jsp"; 
     }
