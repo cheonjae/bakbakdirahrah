40747db6db8174a64a05b41f8fc1eca29563d2b6
juhee-dev <56381189+juhee-dev@users.noreply.github.com>
2021-11-24 14:47:13 +0900

    findBuddyId - 존재하는 모든 채팅방의 상대방 id 반환

--- project-DongneBook/src/main/java/model/dao/ChatDAO.java
+++ project-DongneBook/src/main/java/model/dao/ChatDAO.java
@@ -40,4 +40,29 @@ public class ChatDAO {
 		return null;
 	}
 	
+	public List<String> findBuddyId(String userId) {
+//		중복값 제외하는 방법 있을까?
+		String sql = "SELECT sender_id, receiver_id "
+				   + "FROM chat "
+				   + "WHERE sender_id=? OR receiver_id=?";
+		jdbcUtil.setSqlAndParameters(sql, new Object[] {userId, userId});	
+		
+		try {
+			ResultSet rs = jdbcUtil.executeQuery();				
+			List<String> buddyIdList = new ArrayList<String>();	
+			while (rs.next()) {
+				buddyIdList.add(rs.getString("sender_id"));
+				buddyIdList.add(rs.getString("receiver_id"));
+				// 현재 로그인한 유저 정보를 controller에서 확인할 수 있기 때문에 sender가 상대방인지 receiver가 상대방인지 확인하는 if문은 controller 안에서 돌려야될듯
+				// 일단 sender_id와 receiver_id 모두 반환하고 컨트롤러에서 상대방id 저장
+			}	
+			return buddyIdList;					
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			jdbcUtil.close();
+		}
+		return null;
+	}
+	
 }
