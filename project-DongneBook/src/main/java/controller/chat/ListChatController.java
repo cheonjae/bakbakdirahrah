838e757b6913bc4dd75b9ca869fa29b1d531ed3f
juhee-dev <56381189+juhee-dev@users.noreply.github.com>
2021-11-24 22:31:34 +0900

    Update ListChatController.java

--- project-DongneBook/src/main/java/controller/chat/ListChatController.java
+++ project-DongneBook/src/main/java/controller/chat/ListChatController.java
@@ -1,14 +1,23 @@
-package controller.chat;
+package 
+controller.chat
+
+;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import controller.Controller;
+import controller.user.UserSessionUtils;
 import model.service.ChatManager;
 
 public class ListChatController implements Controller {
+    private static final Logger log = LoggerFactory.getLogger(ListChatController.class);
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -16,15 +25,15 @@ public class ListChatController implements Controller {
     	ChatManager manager = ChatManager.getInstance();
     	String userId = request.getParameter("userId");
 
-    	List<String> buddyList = manager.findBuddyId(userId);
+    	List<String> buddyList = manager.findBuddyId(UserSessionUtils.getLoginUserId(request.getSession()));
     	for (String id : buddyList) {
     		if (id.equals(userId)) {
     			buddyList.remove(id);
     		}
     	}
+    	buddyList = buddyList.stream().distinct().collect(Collectors.toList());
 			
-    	request.setAttribute("user", userId);
 		request.setAttribute("buddyList", buddyList);
 		return "/chat/chatList.jsp"; 
     }
-}
+} 
