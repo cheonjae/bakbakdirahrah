d3644bf55d9eadecb1c2e7d791c4a8fad6d6657c
cheonjae <69137343+cheonjae@users.noreply.github.com>
2021-11-26 16:20:04 +0900

    sellerCheck, buyerCheck 추가

--- project-DongneBook/src/main/java/model/Transaction.java
+++ project-DongneBook/src/main/java/model/Transaction.java
@@ -2,30 +2,54 @@ package model;
 
 public class Transaction {
 
+	private int bookId;
+	private String sellerId;
+	private String buyerId;
 	private int lastPrice;
 	private String meetingDate;
 	private String meetingPlace;
 	private String meetingMemo;
-	private int bookId;
-	private String sellerId;
-	private String buyerId;
 	private int sellerCheck;
 	private int buyerCheck;
 	
 	public Transaction() {	}
 
-	public Transaction(int lastPrice, String meetingDate, String meetingPlace, String meetingMemo, int bookId,
-			String sellerId, String buyerId, int sellerCheck, int buyerCheck) {
+	public Transaction(int bookId, String sellerId, String buyerId, int lastPrice, String meetingDate,
+			String meetingPlace, String meetingMemo, int sellerCheck, int buyerCheck) {
 		super();
+		this.bookId = bookId;
+		this.sellerId = sellerId;
+		this.buyerId = buyerId;
 		this.lastPrice = lastPrice;
 		this.meetingDate = meetingDate;
 		this.meetingPlace = meetingPlace;
 		this.meetingMemo = meetingMemo;
-		this.bookId = bookId;
-		this.sellerId = sellerId;
-		this.buyerId = buyerId;
 		this.sellerCheck = sellerCheck;
 		this.buyerCheck = buyerCheck;
+	}
+
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getSellerId() {
+		return sellerId;
+	}
+
+	public void setSellerId(String sellerId) {
+		this.sellerId = sellerId;
+	}
+
+	public String getBuyerId() {
+		return buyerId;
+	}
+
+	public void setBuyerId(String buyerId) {
+		this.buyerId = buyerId;
 	}
 
 	public int getLastPrice() {
@@ -59,44 +83,21 @@ public class Transaction {
 	public void setMeetingMemo(String meetingMemo) {
 		this.meetingMemo = meetingMemo;
 	}
-	
-	public int getBookId() {
-		return bookId;
-	}
 
-	public void setBookId(int bookId) {
-		this.bookId = bookId;
-	}
-
-	public String getSellerId() {
-		return sellerId;
-	}
-
-	public void setSellerId(String sellerId) {
-		this.sellerId = sellerId;
-	}
-
-	public String getBuyerId() {
-		return buyerId;
-	}
-
-	public void setBuyerId(String buyerId) {
-		this.buyerId = buyerId;
-	}
-	
 	public int getSellerCheck() {
 		return sellerCheck;
 	}
-	
+
 	public void setSellerCheck(int sellerCheck) {
 		this.sellerCheck = sellerCheck;
 	}
-	
+
 	public int getBuyerCheck() {
 		return buyerCheck;
 	}
-	
+
 	public void setBuyerCheck(int buyerCheck) {
 		this.buyerCheck = buyerCheck;
 	}
+	
 }
