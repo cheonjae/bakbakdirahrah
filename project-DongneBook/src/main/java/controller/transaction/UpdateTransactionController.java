9366225688c64f4c278f84a595b226ead444dcce
cheonjae <69137343+cheonjae@users.noreply.github.com>
2021-11-26 21:14:09 +0900

    bookUpdate 참고하여 작성. 구현 안된 부분은 주석처리 해둠

--- project-DongneBook/src/main/java/controller/transaction/UpdateTransactionController.java
+++ project-DongneBook/src/main/java/controller/transaction/UpdateTransactionController.java
@@ -1,5 +1,46 @@
 package controller.transaction;
 
-public class UpdateTransactionController {
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Controller;
+import model.Transaction;
+import model.service.TransactionManager;
+
+public class UpdateTransactionController implements Controller{
+	
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    	
+		if (request.getMethod().equals("GET")) {	
+	    		int bookId = Integer.parseInt(request.getParameter("bookId")); 
+	    		String sellerId = request.getParameter("sellerId");
+	    		String buyerId = request.getParameter("buyerId");
+			
+			TransactionManager manager = TransactionManager.getInstance();
+//			Transaction transaction = manager.findTransactionDetails(bookId, sellerId, buyerId);	
+	
+//			request.setAttribute("transaction", transaction);		
+			return "/transaction/transactionUpdateForm.jsp";
+		}
+		
+//		int bookId = Integer.parseInt(request.getParameter("bookId"));
+//		String sellerId = request.getParameter("sellerId");
+//		String buyerId = request.getParameter("buyerId");
+		int lastPrice = Integer.parseInt(request.getParameter("lastPrice"));
+		String meetingDate = request.getParameter("meetingDate");
+		String meetingPlace = request.getParameter("meetingPlace");
+		String meetingMemo = request.getParameter("meetingMemo");
+		
+
+    	Transaction updateTransaction = new Transaction(lastPrice, meetingDate, meetingPlace, meetingMemo);
+
+    	TransactionManager manager = TransactionManager.getInstance();
+		manager.update(updateTransaction);
+		
+		request.setAttribute("transaction", updateTransaction);
+
+		return "/transaction/transactionView.jsp";
+    }	 	
 }
