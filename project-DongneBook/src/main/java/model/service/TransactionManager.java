2ddfc2f5be58a1b32cad8ec6c2a0ebc5f9e1b987
Yujin <91461061+HeoYuJinn@users.noreply.github.com>
2021-11-27 00:49:20 +0900

    deleteTransaction 추가~
    import하는거나 다른거 수정할거있으면 자유롭게~ 저하늘을~

--- project-DongneBook/src/main/java/model/service/TransactionManager.java
+++ project-DongneBook/src/main/java/model/service/TransactionManager.java
@@ -1,5 +1,31 @@
 package model.service;
 
-public class TransactionManager {
+import java.sql.SQLException;
+import java.util.List;
 
+import model.Transaction;
+import model.dao.BookDAO;
+import model.dao.TransactionDAO;
+
+
+public class TransactionManager {
+	private static TransactionManager tmanager = new TransactionManager();
+	private TransactionDAO transactionDAO;
+	
+	private TransactionManager() {
+		try {
+			transactionDAO = new TransactionDAO();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}			
+	}
+	
+	public static TransactionManager getInstance() {
+		return tmanager;
+	}
+	
+	public int deleteTransaction(int bookId, String sellerId, String buyerId) throws SQLException, BookNotFoundException {
+	
+		return transactionDAO.deleteTransaction(bookId,sellerId,buyerId);
+	}
 }
