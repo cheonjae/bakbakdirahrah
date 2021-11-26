e86e0ec35dfc482fcfb908f495c3f8fb9dcf5e50
Yujin <91461061+HeoYuJinn@users.noreply.github.com>
2021-11-27 00:52:19 +0900

    Update DeleteTransactionController.java

--- project-DongneBook/src/main/java/controller/transaction/DeleteTransactionController.java
+++ project-DongneBook/src/main/java/controller/transaction/DeleteTransactionController.java
@@ -1,5 +1,31 @@
 package controller.transaction;
 
-public class DeleteTransactionController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import controller.Controller;
+import controller.book.DeleteBookController;
+import controller.user.UserSessionUtils;
+import model.Book;
+import model.User;
+import model.service.TransactionManager;
+
+public class DeleteTransactionController implements Controller{
+	private static final Logger log = LoggerFactory.getLogger(DeleteTransactionController.class);
+	 	@Override
+	    public String execute(HttpServletRequest request, HttpServletResponse response)	throws Exception {
+	 	
+	 		int deletebookId = Integer.parseInt(request.getParameter("bookId"));
+	 		String deletesellerId = request.getParameter("sellerId");
+	 		String deletbuyerId = request.getParameter("buyerId");
+	 
+	 
+	 		TransactionManager tmanager = TransactionManager.getInstance();
+	 		tmanager.deleteTransaction(deletebookId, deletesellerId, deletbuyerId); //도대체 여기 오류 왜 나는거죠? 순서 맞는데? int String String 일단 올립니다..
+		 return "redirect:/user/mypage";
+	 }
 }
