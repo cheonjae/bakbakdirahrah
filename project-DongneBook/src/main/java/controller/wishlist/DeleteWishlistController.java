a9c20b2d046b77f25d25380c6d6d3e77cf126097
Yujin <91461061+HeoYuJinn@users.noreply.github.com>
2021-11-28 00:29:00 +0900

    Update DeleteWishlistController.java

--- project-DongneBook/src/main/java/controller/wishlist/DeleteWishlistController.java
+++ project-DongneBook/src/main/java/controller/wishlist/DeleteWishlistController.java
@@ -1,5 +1,34 @@
 package controller.wishlist;
 
-public class DeleteWishlistController {
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import controller.Controller;
+import model.Wishlist;
+import model.service.BookManager;
+import model.service.ExistingUserException;
+import model.service.WishlistManager;
+
+public class DeleteWishlistController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		String userId =  request.getParameter("userId");
+		int bookId = Integer.parseInt(request.getParameter("bookId"));
+		
+		//Wishlist wishlist = new Wishlist(userId, bookId);
+		
+		WishlistManager wishlistmanager = WishlistManager.getInstance();
+		wishlistmanager.delete(userId, bookId);
+		
+
+		return null;
+	}
 
 }
