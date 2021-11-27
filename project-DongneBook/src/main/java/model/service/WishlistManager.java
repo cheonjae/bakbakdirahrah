4da0806a9f464733a109c45713f8c2bbf5de73f0
Yujin <91461061+HeoYuJinn@users.noreply.github.com>
2021-11-28 00:30:04 +0900

    Update WishlistManager.java

--- project-DongneBook/src/main/java/model/service/WishlistManager.java
+++ project-DongneBook/src/main/java/model/service/WishlistManager.java
@@ -1,5 +1,32 @@
 package model.service;
 
-public class WishlistManager {
+import java.sql.SQLException;
+import model.*;
+import model.dao.BookDAO;
+import model.dao.WishlistDAO;
 
+public class WishlistManager {
+	private static WishlistManager wishMan = new WishlistManager();
+	private WishlistDAO wishDAO;
+
+	private WishlistManager() {
+		try {
+			wishDAO = new WishlistDAO();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}			
+	}
+	
+	public static WishlistManager getInstance() {
+		return wishMan;
+	}
+	
+	public int add(Wishlist wishlist) throws SQLException {
+		return wishDAO.add(wishlist);
+		
+	}
+	
+	public int delete(String userId, int bookId) throws SQLException {
+		return wishDAO.delete(userId, bookId);
+	}
 }
