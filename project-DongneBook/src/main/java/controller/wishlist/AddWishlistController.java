ae7d4698e176430979c6876dbabef4f604b06f9d
Yujin <91461061+HeoYuJinn@users.noreply.github.com>
2021-11-28 00:28:38 +0900

    Update AddWishlistController.java
    일단 main으로 가게해놈

--- project-DongneBook/src/main/java/controller/wishlist/AddWishlistController.java
+++ project-DongneBook/src/main/java/controller/wishlist/AddWishlistController.java
@@ -1,5 +1,32 @@
 package controller.wishlist;
 
-public class AddWishlistController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import controller.Controller;
+import model.Wishlist;
+import model.service.ExistingUserException;
+import model.service.WishlistManager;
+
+public class AddWishlistController implements Controller{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		String userId =  request.getParameter("userId");
+		int bookId = Integer.parseInt(request.getParameter("bookId"));
+		
+		Wishlist wishlist = new Wishlist(userId, bookId);
+		
+		WishlistManager wishlistmanager = WishlistManager.getInstance();
+		wishlistmanager.add(wishlist);
+		
+
+		return "redirect:/user/main";
+	}
 
 }
