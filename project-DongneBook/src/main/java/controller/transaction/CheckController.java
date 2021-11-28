e2ddf5d8851f9d6f94e110f12f0aecbd900e8b42
cheonjae <69137343+cheonjae@users.noreply.github.com>
2021-11-29 03:40:28 +0900

    Update CheckController.java

--- project-DongneBook/src/main/java/controller/transaction/CheckController.java
+++ project-DongneBook/src/main/java/controller/transaction/CheckController.java
@@ -1,6 +1,9 @@
 package controller.transaction;
 
 import controller.Controller;
+
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -18,34 +21,42 @@ import model.service.UserManager;
 
 public class CheckController implements Controller {
 	private static final Logger log = LoggerFactory.getLogger(CheckController.class);
-    
+
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception { 	
     	TransactionManager tmanager = TransactionManager.getInstance();
     	UserManager umanager = UserManager.getInstance();
-    	
+
     	int bookId = Integer.parseInt(request.getParameter("bookId"));
     	String userId = request.getParameter("userId");
     	String sellerId = request.getParameter("sellerId");
-    	
+
     	log.debug("CheckController - bookId : {}", bookId);
     	log.debug("CheckController - userId : {}", userId);
     	log.debug("CheckController - sellerId : {}", sellerId);
-    	
+
     	tmanager.checkUpdate(bookId, userId, sellerId);
-    	
+
     	User user = null;
 		user = umanager.findUser(userId);
-		
+
     	Transaction transaction = null;
     	transaction = tmanager.view(bookId, userId, sellerId);
-		
+
 		log.debug("transaction : {}", transaction);
 		
+		List<Transaction> sellDetail = tmanager.sellDetail(UserSessionUtils.getLoginUserId(request.getSession()), bookId);
+
+		request.setAttribute("bookId", bookId);
     	request.setAttribute("user", user);			
 		request.setAttribute("sellerId", sellerId);
 		request.setAttribute("transaction", transaction);
-		
-		return "/transaction/transactionView.jsp"; 
+		request.setAttribute("sellDetail",  sellDetail);
+
+		if(request.getServletPath().equals("/transaction/check")) {
+			return "/transaction/transactionView.jsp"; 
+		} else {
+			return "/mypage/sellDetail.jsp";
+		}
     }
 }
