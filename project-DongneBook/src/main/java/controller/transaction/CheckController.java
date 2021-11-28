32ffe982cc08f1b022e5cbce0c1b3acead3311d7
이가은 <67837142+chashudong@users.noreply.github.com>
2021-11-29 04:03:34 +0900

    Update CheckController.java

--- project-DongneBook/src/main/java/controller/transaction/CheckController.java
+++ project-DongneBook/src/main/java/controller/transaction/CheckController.java
@@ -29,33 +29,35 @@ public class CheckController implements Controller {
 
     	int bookId = Integer.parseInt(request.getParameter("bookId"));
     	String userId = request.getParameter("userId");
-    	String sellerId = request.getParameter("sellerId");
+    	String buddyId = request.getParameter("buddyId");
 
     	log.debug("CheckController - bookId : {}", bookId);
     	log.debug("CheckController - userId : {}", userId);
-    	log.debug("CheckController - sellerId : {}", sellerId);
+    	log.debug("CheckController - buddyId : {}", buddyId);
 
-    	tmanager.checkUpdate(bookId, userId, sellerId);
+    	tmanager.checkUpdate(bookId, userId, buddyId);
 
     	User user = null;
 		user = umanager.findUser(userId);
 
     	Transaction transaction = null;
-    	transaction = tmanager.view(bookId, userId, sellerId);
+    	transaction = tmanager.view(bookId, userId, buddyId);
 
 		log.debug("transaction : {}", transaction);
 		
 		List<Transaction> sellDetail = tmanager.sellDetail(UserSessionUtils.getLoginUserId(request.getSession()), bookId);
 
 		request.setAttribute("bookId", bookId);
-    	request.setAttribute("user", user);			
-		request.setAttribute("sellerId", sellerId);
-		request.setAttribute("transaction", transaction);
-		request.setAttribute("sellDetail",  sellDetail);
 
 		if(request.getServletPath().equals("/transaction/check")) {
+			request.setAttribute("user", user);			
+			request.setAttribute("buddyId", buddyId);
+			request.setAttribute("transaction", transaction);
+			
 			return "/transaction/transactionView.jsp"; 
 		} else {
+			request.setAttribute("sellDetail",  sellDetail);
+			
 			return "/mypage/sellDetail.jsp";
 		}
     }
