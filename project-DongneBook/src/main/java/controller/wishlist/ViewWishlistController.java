f9550e77a91b73793ab58d80b9b40b59bb051e2c
juhee-dev <56381189+juhee-dev@users.noreply.github.com>
2021-11-29 01:43:06 +0900

    Update ViewWishlistController.java

--- project-DongneBook/src/main/java/controller/wishlist/ViewWishlistController.java
+++ project-DongneBook/src/main/java/controller/wishlist/ViewWishlistController.java
@@ -1,5 +1,36 @@
 package controller.wishlist;
 
-public class ViewWishlistController {
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import controller.Controller;
+import controller.chat.CreateChatController;
+import controller.user.UserSessionUtils;
+import model.Book;
+import model.service.WishlistManager;
+
+public class ViewWishlistController implements Controller {
+	private static final Logger log = LoggerFactory.getLogger(CreateChatController.class);
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		WishlistManager manager = WishlistManager.getInstance();
+		String userId = UserSessionUtils.getLoginUserId(request.getSession());
+		log.debug("wishlist: user id = "+ userId);
+		
+		List<Book> bookList = manager.wishBookList(userId);
+		for (Book book : bookList)
+			System.out.println(book.getTitle());
+
+		request.setAttribute("wishBookList", bookList);
+		
+		return "/user/wishlist.jsp";
+	}
 
 }
