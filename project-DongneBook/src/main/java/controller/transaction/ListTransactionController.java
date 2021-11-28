8762dc85a2e92923b5d9f11269d09a800e76be6f
cheonjae <69137343+cheonjae@users.noreply.github.com>
2021-11-29 03:43:36 +0900

    Add files via upload

---
+++ project-DongneBook/src/main/java/controller/transaction/ListTransactionController.java
@@ -0,0 +1,41 @@
+package controller.transaction;
+
+import controller.Controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import controller.user.UserSessionUtils;
+import model.Book;
+import model.Transaction;
+import model.service.BookManager;
+import model.service.TransactionManager;
+
+public class ListTransactionController implements Controller {
+	private static final Logger log = LoggerFactory.getLogger(ListTransactionController.class);
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception { 	
+		
+		// buyList는 트잭 써야해서 트잭에 만들었음
+		TransactionManager tmanager = TransactionManager.getInstance();
+		List<Transaction> buyList = tmanager.buyList(UserSessionUtils.getLoginUserId(request.getSession()));
+		
+		// sellList는 그냥 책만 가져오면 돼서 
+		BookManager bmanager = BookManager.getInstance();	
+		List<Book> sellList = bmanager.sellList(UserSessionUtils.getLoginUserId(request.getSession()));
+		
+		if(request.getServletPath().equals("/transaction/sell")) {
+			request.setAttribute("sellList", sellList);
+		} else {
+			request.setAttribute("buyList", buyList);
+		}
+
+		
+	return "/mypage/history.jsp";
+	}
+}
