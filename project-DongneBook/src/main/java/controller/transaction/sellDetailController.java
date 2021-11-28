787f995837de18d9acc016e2257f190cf2a01f79
cheonjae <69137343+cheonjae@users.noreply.github.com>
2021-11-29 03:45:00 +0900

    Add files via upload

---
+++ project-DongneBook/src/main/java/controller/transaction/sellDetailController.java
@@ -0,0 +1,35 @@
+package controller.transaction;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import controller.Controller;
+import controller.user.UserSessionUtils;
+import model.Transaction;
+import model.service.TransactionManager;
+
+public class sellDetailController implements Controller {
+	private static final Logger log = LoggerFactory.getLogger(sellDetailController.class);
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    	
+    	log.debug("1");
+    	
+        int bookId = Integer.parseInt(request.getParameter("bookId"));
+        
+        log.debug("bookId : {} ", bookId);
+    	
+    	TransactionManager manager = TransactionManager.getInstance();
+        List<Transaction> sellDetail = manager.sellDetail(UserSessionUtils.getLoginUserId(request.getSession()), bookId);
+    		
+        	request.setAttribute("bookId", bookId);
+        	request.setAttribute("sellDetail", sellDetail);					
+    	return "/mypage/sellDetail.jsp"; 
+    }
+}
