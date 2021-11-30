f7564a1031b064d77566d2003566686c98dffd06
cheonjae <69137343+cheonjae@users.noreply.github.com>
2021-12-01 00:33:24 +0900

    Update AddWishlistController.java

--- project-DongneBook/src/main/java/controller/wishlist/AddWishlistController.java
+++ project-DongneBook/src/main/java/controller/wishlist/AddWishlistController.java
@@ -1,15 +1,16 @@
 package controller.wishlist;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import controller.Controller;
+import model.Book;
 import model.Wishlist;
-import model.service.ExistingUserException;
 import model.service.WishlistManager;
 
 public class AddWishlistController implements Controller{
