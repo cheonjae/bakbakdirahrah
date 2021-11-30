5dff0a0bc0d017ac7e1693fad8a90f3ca1b9ada4
cheonjae <69137343+cheonjae@users.noreply.github.com>
2021-12-01 00:34:48 +0900

    Update DeleteWishlistController.java

--- project-DongneBook/src/main/java/controller/wishlist/DeleteWishlistController.java
+++ project-DongneBook/src/main/java/controller/wishlist/DeleteWishlistController.java
@@ -1,17 +1,16 @@
 package controller.wishlist;
 
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import controller.Controller;
-import model.Wishlist;
-import model.service.BookManager;
-import model.service.ExistingUserException;
+import model.Book;
 import model.service.WishlistManager;
 
 public class DeleteWishlistController implements Controller {
