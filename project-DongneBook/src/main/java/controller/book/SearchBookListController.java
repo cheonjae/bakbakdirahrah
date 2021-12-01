7f2e8abd42cabab3d82da8f795756af970e551cd
이가은 <67837142+chashudong@users.noreply.github.com>
2021-12-01 17:49:50 +0900

    Update SearchBookListController.java

--- project-DongneBook/src/main/java/controller/book/SearchBookListController.java
+++ project-DongneBook/src/main/java/controller/book/SearchBookListController.java
@@ -3,6 +3,8 @@ package controller.book;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import controller.Controller;
 import controller.user.UserSessionUtils;
 import model.Book;
@@ -14,24 +16,25 @@ public class SearchBookListController implements Controller {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response)	throws Exception {
     	BookManager bmanager = BookManager.getInstance();
-	String title = request.getParameter("title");
-		
-    	String userId = UserSessionUtils.getLoginUserId(request.getSession());
-
+    	String title = request.getParameter("title");    	
+    	HttpSession session = request.getSession();	
+    	
     	List<Book> bookList = null;
-    	if (userId == null) {
+    	if (!UserSessionUtils.hasLogined(session)) {
     		bookList = bmanager.searchBookList(title, "구");
     	}
     	else {
+    		String userId = UserSessionUtils.getLoginUserId(request.getSession());
+    		
     		UserManager umanager = UserManager.getInstance();
         	User user = umanager.findUser(userId);
         	
     		bookList = bmanager.searchBookList(title, user.getLocation());
     	} 
 		
-	request.setAttribute("bookList", bookList);
-	request.setAttribute("searchWord", title);	
+    	request.setAttribute("bookList", bookList);
+    	request.setAttribute("searchWord", title);	
 	
-	return "/user/main.jsp";   
+    	return "/user/main.jsp";   
     }
 }
