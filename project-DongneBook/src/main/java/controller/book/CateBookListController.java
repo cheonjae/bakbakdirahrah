44921a4b20f1d49c53402609e7bb610ca41786f8
이가은 <67837142+chashudong@users.noreply.github.com>
2021-12-01 17:52:28 +0900

    Update CateBookListController.java

--- project-DongneBook/src/main/java/controller/book/CateBookListController.java
+++ project-DongneBook/src/main/java/controller/book/CateBookListController.java
@@ -3,6 +3,8 @@ package controller.book;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import controller.Controller;
 import controller.user.UserSessionUtils;
 import model.Book;
@@ -14,23 +16,24 @@ public class CateBookListController implements Controller {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response)	throws Exception {
     	BookManager bmanager = BookManager.getInstance();
-	String cateId = request.getParameter("cateId");
-		
-    	String userId = UserSessionUtils.getLoginUserId(request.getSession());
-
+    	String cateId = request.getParameter("cateId");
+    	HttpSession session = request.getSession();	
+    	
     	List<Book> bookList = null;
-    	if (userId == null) {
+    	if (!UserSessionUtils.hasLogined(session)) {
     		bookList = bmanager.cateBookList(Integer.parseInt(cateId), "구");
     	}
     	else {
+    		String userId = UserSessionUtils.getLoginUserId(request.getSession());
+
     		UserManager umanager = UserManager.getInstance();
         	User user = umanager.findUser(userId);
         	
     		bookList = bmanager.cateBookList(Integer.parseInt(cateId), user.getLocation());
     	} 
 		
-	// bookList 객체를 request에 저장하여 메인 화면으로 이동(forwarding)
-	request.setAttribute("bookList", bookList);				
-	return "/user/main.jsp";   
+    	// bookList 객체를 request에 저장하여 메인 화면으로 이동(forwarding)
+    	request.setAttribute("bookList", bookList);				
+    	return "/user/main.jsp";   
     }
 }
