251bb08c870b78e45806a72630b98d2d68e20829
이가은 <67837142+chashudong@users.noreply.github.com>
2021-12-21 21:50:37 +0900

    Update UpdateTransactionController.java

--- project-DongneBook/src/main/java/controller/transaction/UpdateTransactionController.java
+++ project-DongneBook/src/main/java/controller/transaction/UpdateTransactionController.java
@@ -14,49 +14,51 @@ import model.service.TransactionManager;
 public class UpdateTransactionController implements Controller{
 	private static final Logger log = LoggerFactory.getLogger(ViewTransactionController.class);
 	
-    @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    	@Override
+    	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
     	
 		if (request.getMethod().equals("GET")) {	
 	    		int bookId = Integer.parseInt(request.getParameter("bookId")); 
-	    		String sellerId = request.getParameter("sellerId");
+	    		String buddyId = request.getParameter("buddyId");
 	    		String buyerId = request.getParameter("buyerId");
 	    	
 			TransactionManager manager = TransactionManager.getInstance();
-			Transaction transaction = manager.view(bookId, sellerId, buyerId);	
+			Transaction transaction = manager.view(bookId, buddyId, buyerId);	
 			
-			log.debug("bookId: {} sellerId: {} buyerId: {}", bookId, sellerId, buyerId);
+			log.debug("bookId: {} buddyId: {} buyerId: {}", bookId, buddyId, buyerId);
 			
-			request.setAttribute("sellerId", sellerId);
+			request.setAttribute("buddyId", buddyId);
 			request.setAttribute("transaction", transaction);		
 			return "/transaction/transactionUpdateForm.jsp";
 		}
 		
 		int bookId = Integer.parseInt(request.getParameter("bookId"));
-		String sellerId = request.getParameter("sellerId");
+		String buddyId = request.getParameter("buddyId");
 		String buyerId = request.getParameter("buyerId");
 		int lastPrice = Integer.parseInt(request.getParameter("lastPrice"));
 		String meetingDate = request.getParameter("meetingDate");
 		String meetingPlace = request.getParameter("meetingPlace");
 		String meetingMemo = request.getParameter("meetingMemo");
 		
-		log.debug("bookId: {} sellerId: {} buyerId: {} lastPrice: {} meetingDate: {} meetingPlace: {} meetingMemo: {}"
-				, bookId, sellerId, buyerId, lastPrice, meetingDate, meetingPlace,  meetingMemo);
+		log.debug("bookId: {} buddyId: {} buyerId: {} lastPrice: {} meetingDate: {} meetingPlace: {} meetingMemo: {}"
+				, bookId, buddyId, buyerId, lastPrice, meetingDate, meetingPlace,  meetingMemo);
 		
 
 	    	TransactionManager manager = TransactionManager.getInstance();
-	    	Transaction createTransaction = new Transaction(bookId, sellerId, buyerId, lastPrice, meetingDate, meetingPlace, meetingMemo, 0, 0);	
-	    	Transaction updateTransaction = new Transaction(bookId, sellerId, buyerId, lastPrice, meetingDate, meetingPlace, meetingMemo);
+	   	Transaction createTransaction = new Transaction(bookId, buddyId, buyerId, lastPrice, meetingDate, meetingPlace, meetingMemo, 0, 0);	
+	    	Transaction updateTransaction = new Transaction(bookId, buddyId, buyerId, lastPrice, meetingDate, meetingPlace, meetingMemo);
     	
-		if(manager.view(bookId, sellerId, buyerId) == null) {
+		if(manager.view(bookId, buddyId, buyerId) == null) {
 			manager.create(createTransaction);
 		}
 		else {
 			manager.update(updateTransaction);
 		}
 		
-		Transaction transaction = manager.view(bookId, sellerId, buyerId);
+		Transaction transaction = manager.view(bookId, buddyId, buyerId);
+		
 		request.setAttribute("transaction", transaction);
+
 		return "/transaction/transactionView.jsp";
-    }	 	
+	}	 	
 }
