eb41edb8d4121358a795aba2b776f7f06a85b41e
juhee-dev <56381189+juhee-dev@users.noreply.github.com>
2021-12-22 17:06:11 +0900

    Update MainBookListController.java

--- project-DongneBook/src/main/java/controller/book/MainBookListController.java
+++ project-DongneBook/src/main/java/controller/book/MainBookListController.java
@@ -25,7 +25,7 @@ public class MainBookListController implements Controller {
     	
     	List<Book> bookList = null;
     	if (!UserSessionUtils.hasLogined(session)) {
-    		bookList = bmanager.mainBookList("±¸");
+    		bookList = bmanager.mainBookList("êµ¬");
     	}
     	else {
     		String userId = UserSessionUtils.getLoginUserId(request.getSession());
